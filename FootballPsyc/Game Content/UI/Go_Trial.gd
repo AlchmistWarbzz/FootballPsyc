921938f82ca9fe880869351e4d1a9e040ab7f998
JarodShorrockGames <jarod.shorrock@gmail.com>
2024-04-29 10:47:15 +0100

    Level Updated with VR Perspective Test and UI Updates for Future Testing

--- FootballPsyc/Game Content/UI/Go_Trial.gd
+++ FootballPsyc/Game Content/UI/Go_Trial.gd
@@ -1,13 +1,13 @@
 extends Node
 
 
+
 @onready var anim = $"."
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.play("GoTrial")
-
+	anim.play ("GoTrial")
 
 
 
