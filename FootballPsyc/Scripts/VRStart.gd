f396ed5b5ad3aecd277674019268d61de1b703e8
AlchmistWarbzz <ALchemistwarbzz@gmail.com>
2024-07-17 01:53:15 +0100

    Get collider name on button press

--- FootballPsyc/Scripts/VRStart.gd
+++ FootballPsyc/Scripts/VRStart.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 var xr_interface: XRInterface
-
+var target #= $XROrigin3D/RightHand/FunctionPointer/RayCast.get_collider()
 
 
 func _ready():
@@ -28,15 +28,21 @@ func _on_left_hand_button_pressed(name):
 	#print("PRESSED LEFT TRIGGER")
 	LevelManager.left_trigger.emit()
 
+
 func _on_right_hand_button_pressed(name):
 	#print("PRESSED RIGHT TRIGGER")
+	target = $XROrigin3D/RightHand/FunctionPointer/RayCast.get_collider()
 	LevelManager.right_trigger.emit()
-
-
-func _on_function_pointer_pointing_event(event):
-	var target = $XROrigin3D/RightHand/FunctionPointer/RayCast.get_collider()
 	if target:
 		print("This is ", target.name)
 	else:
 		print("No target hit")
-	
+
+
+#func _on_function_pointer_pointing_event(event):
+	target = $XROrigin3D/RightHand/FunctionPointer/RayCast.get_collider()
+	#if target:
+		#print("This is ", target.name)
+	#else:
+		#print("No target hit")
+	#
